e324baeb0602ea262b7cca18f1993e0186c95286
Andrew Hale <andyhalebmx@hotmail.com>
2019-07-16 10:06:27 +0100

    initial commit

---
+++ js/app.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+    $(".reveal-page").delay(100).animate({ opacity: 1 }, 500);
+    // $("#typewriter").css("background-color", "gray");
+    // setTimeout(function () {
+    //     $("#typewriter").css("background", "yellow");
+    // }, 3050);
+});
+
+$(function () {
+    $('#main-title').animate({ opacity: "1" }, 500);
+});
+
+$(function () {
+    $('#about-title').animate({ left: 0, opacity: "1" }, 500);
+});
+
+$(function () {
+    $('#contact-title').animate({ top: 0, opacity: "1" }, 500);
+});
+
+const typewriter = new Typewriter('#typewriter', {
+    autoStart: true,
+    loop: true,
+});
+
+typewriter
+    .typeString('HTML / S(CSS)')
+    .pauseFor(500)
+    .deleteAll(30)
+    .start()
+    .typeString('Javascript')
+    .pauseFor(500)
+    .deleteAll(30)
+    .start()
+    .typeString('Wordpress')
+    .pauseFor(500)
+    .deleteAll(30)
+    .start()
+    .typeString('PHP')
+    .pauseFor(500)
+    .deleteAll(30);
+
+//# sourceMappingURL=app.js.map
